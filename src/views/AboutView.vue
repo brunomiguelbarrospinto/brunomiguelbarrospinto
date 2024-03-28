1ef48d27d86b266c1dfc1498ecd1a811e4cedd88
Bruno Barros <brunomiguelbarrospinto@gmail.com>
2024-03-28 21:08:06 +0100

    First commit - Vue 3 start template

---
+++ src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Count: {{ counterStore.count }}</p>
+    <p>Double count: {{ counterStore.doubleCount }}</p>
+    <button @click="counterStore.increment">Increment</button>
+    <button @click="counterStore.$reset()">Reset store</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const counterStore = useCounterStore()
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
